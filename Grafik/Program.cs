7677f18e839fd61f455a1edd0ec28f2367cd8502
Mikael <mikael.bergstrom@ga.ntig.se>
2020-11-04 14:04:05 +0100

    Initial commit

---
+++ Grafik/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grafik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
