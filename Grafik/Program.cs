cc421cb25ad33d33b07c9f463c7a8ec2b8855d27
Mikael <mikael.bergstrom@ga.ntig.se>
2020-11-08 02:02:58 +0100

    Implemented graphics

--- Grafik/Program.cs
+++ Grafik/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using Raylib_cs;
 
 namespace Grafik
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      Raylib.InitWindow(800, 600, "Grafikdemo");
+
+      Color hotPink = new Color(255, 105, 180, 255);
+
+      float x = 30;
+
+      while (!Raylib.WindowShouldClose())
+      {
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
         {
-            Console.WriteLine("Hello World!");
+          x += 0.05f;
         }
+
+
+
+        Raylib.BeginDrawing();
+
+        Raylib.ClearBackground(hotPink);
+
+        Raylib.DrawRectangle((int) x, 10, 20, 20, Color.BLACK);
+
+        Raylib.EndDrawing();
+
+      }
+
+
     }
+  }
 }
